f4becd1236adaa621afdc8893b9e65c815090163
marbypangilinan <marbysneaker@gmail.com>
2023-04-13 00:12:21 -0700

    finish designing grid

--- app/src/main/java/com/example/hw5game/MainActivity.java
+++ app/src/main/java/com/example/hw5game/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.hw5game;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewTime, textViewCountDown, textViewScore;
+    private ImageView balloon1,balloon2,balloon3,balloon4,balloon5,balloon6,balloon7,balloon8,balloon9;
+    private GridLayout gridLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewTime = findViewById(R.id.textViewTime);
+        textViewCountDown = findViewById(R.id.textViewCountdown);
+        balloon1 = findViewById(R.id.balloon1);
+        balloon2 = findViewById(R.id.balloon2);
+        balloon3 = findViewById(R.id.balloon3);
+        balloon4 = findViewById(R.id.balloon4);
+        balloon5 = findViewById(R.id.balloon5);
+        balloon6 = findViewById(R.id.balloon6);
+        balloon7 = findViewById(R.id.balloon7);
+        balloon8 = findViewById(R.id.balloon8);
+        balloon9 = findViewById(R.id.balloon9);
+        gridLayout = findViewById(R.id.gridLayout);
+
     }
 }
---
+++ app/src/main/java/com/example/hw5game/ResultActivity.java
@@ -0,0 +1,24 @@
+package com.example.hw5game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+    private TextView textViewInfo, textViewMyScore, textViewHighestScore;
+    private Button buttonPlayAgain, buttonQuitGame;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        textViewInfo =findViewById(R.id.textViewInfo);
+        textViewMyScore =findViewById(R.id.textViewMyScore);
+        textViewHighestScore =findViewById(R.id.textViewHighestScore);
+        buttonPlayAgain =findViewById(R.id.buttonPlayAgain);
+        buttonQuitGame =findViewById(R.id.buttonQuitGame);
+
+    }
+}
