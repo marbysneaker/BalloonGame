f581184de264e17d727690eaa36d66d0a1069bff
Marcelino Pangilinan <marbysneaker@gmail.com>
2023-04-15 13:09:16 -0700

    finished game

--- app/src/main/java/com/example/hw5game/ResultActivity.java
+++ app/src/main/java/com/example/hw5game/ResultActivity.java
@@ -2,16 +2,22 @@ package com.example.hw5game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+
 
 public class ResultActivity extends AppCompatActivity {
     private TextView textViewInfo, textViewMyScore, textViewHighestScore;
     private Button buttonPlayAgain, buttonQuitGame;
 
     SharedPreferences sharedPreferences;
+
+    private String sharePreFile = "com.example.hw5game";
 
     int score;
 
@@ -22,6 +28,7 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        getData();
 
         textViewInfo =findViewById(R.id.textViewInfo);
         textViewMyScore =findViewById(R.id.textViewMyScore);
@@ -37,15 +44,36 @@ public class ResultActivity extends AppCompatActivity {
         }
         textViewHighestScore.setText("Highest Score: " + highscore);
 
+        savedData();
 
+        buttonPlayAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ResultActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    public void getData(){
+        sharedPreferences = getSharedPreferences("saveData",MODE_PRIVATE);
+        highscore = sharedPreferences.getInt("highestScore",0);
+    }
+    protected void onPause(){
+        savedData();
+        super.onPause();
     }
 
     public void savedData(){
         sharedPreferences =getSharedPreferences("saveData",MODE_PRIVATE);
-        highestScore = textViewHighestScore.getText().toString();
+//        highestScore = textViewHighestScore.getText().toString();
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("key score", highestScore);
+        editor.putInt("highestScore",highscore);
+        editor.commit();
     }
+
 
 
 
